b92ebc1e5f6fd4def99dc6923fdfe55b5c730567
alex <uzun.alex1@gmail.com>
2023-09-28 05:45:34 -0400

    add 123

--- test.cpp
+++ test.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    std::cout << "Hello World!";
+int main()
+{
+    int i;
+    i = 69;
+    int number = 72;
+    i = 69 + 72;
+    int arr[3] {};
+    int *iPointer {&i};
+
+    
     return 0;
 }
